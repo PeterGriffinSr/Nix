834a261eb5901225b1f235eea8c8b7b4f8c1eb6c
Codezz ops <codezz-ops@protonmail.com>
2025-05-27 03:59:13 -0400

    added workflow

--- examples/main.nix
+++ examples/main.nix
@@ -1,11 +1,1 @@
-add(Int(a), Int(b)) : Int = 
-    a + b;
-
-sub(Int(a), Int(b)) : Int = 
-    a - b;
-
-mul(Int(a), Int(b)) : Int = 
-    a * b;
-
-div(Float(a), Float(b)) : Float =
-    a / b;
+use IO.print
